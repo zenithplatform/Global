87abefededa1e404f4edb74141003adeef7850e6
Civa <civa037@yahoo.com>
2017-01-14 12:59:13 +0100

    Moved shared, caching and data providers to core

--- src/Zenith.Core.Shared/Pipelining/IFilter.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Zenith.Core.Shared.Pipelining
-{
-    public interface IFilter<T>
-    {
-        T Execute(T input);
-        void Register(IFilter<T> nextFilter);
-    }
-}
--- src/Zenith.Core.Shared/Pipelining/IFilterChain.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Zenith.Core.Shared.Pipelining
-{
-    public interface IFilterChain<T>
-    {
-        void Execute(T input);
-        IFilterChain<T> Register(IFilter<T> nextFilter);
-    }
-}
